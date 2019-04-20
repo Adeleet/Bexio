fa94adf220db7135b02651ed177c07773756f2b9
Max Adaloudis <maxadaloudis@gmail.com>
2019-04-20 21:49:55 +0200

    adds download functions for historical quote and trade data

---
+++ generate_data.py
@@ -0,0 +1,15 @@
+import requests
+
+
+def download_data(source):
+    if source not in ["trade", "quote"]:
+        raise ValueError("source should be trade or quote")
+    with open(f"data/{source}_urls.txt") as file_object:
+        file_lines = file_object.readlines()
+        urls = [q.strip() for q in file_lines]
+    sess = requests.Session()
+    for url in urls:
+        file_name = url[url.index(source) + 6:]
+        req = sess.get(url)
+        with open(f"./data/{source}/{file_name}", "wb") as file_object:
+            file_object.write(req.content)
