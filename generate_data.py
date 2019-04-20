6a8456bdb94192f3b6d55d01b737ff692a066360
Max Adaloudis <maxadaloudis@gmail.com>
2019-04-20 22:26:51 +0200

    Adds function to create pandas dataframes for quote and trade

--- generate_data.py
+++ generate_data.py
@@ -1,5 +1,7 @@
 import os
+from glob import glob
 
+import pandas as pd
 import requests
 
 
@@ -18,3 +20,23 @@ def download_data(source):
 
         with open(f"./data/{source}/{file_name}", "wb") as file_object:
             file_object.write(req.content)
+
+
+def create_dataframes():
+    if min(
+            len(glob("data\\quote\\*.csv.gz")),
+            len(glob("data\\quote\\*.csv.gz"))) < 0:
+        raise EnvironmentError(
+            "No data found, please run download_data() first")
+
+    quote_dfs = [pd.read_csv(f"data/quote/{f}")
+                 for f in os.listdir("data/quote")[:10]]
+    pd.concat(quote_dfs).to_csv("data/quote.csv.gz", compression="gzip")
+    quote_dfs = None
+    trade_dfs = [pd.read_csv(f"data/trade/{f}")
+                 for f in os.listdir("data/trade")]
+
+    pd.concat(trade_dfs).to_csv("data/trade.csv.gz", compression="gzip")
+
+
+create_dataframes()
