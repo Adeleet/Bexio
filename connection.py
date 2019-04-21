0d6436397703e730622fb722f046a4653e2599c8
Max Adaloudis <maxadaloudis@gmail.com>
2019-04-21 21:24:49 +0200

    Adds BitMEX Session Class that calls all routes as methods

--- connection.py
+++ connection.py
@@ -1,11 +1,9 @@
 import hmac
 import time
 from hashlib import sha256
-from urllib.parse import quote, urlencode
+from urllib.parse import quote
 
-from requests import request
-
-from config import API_KEY, API_SECRET, BASE_URL
+from config import API_KEY, API_SECRET
 
 
 def generate_nonce():
@@ -27,14 +25,3 @@ def generate_headers(method, endpoint, data=""):
         'api-key': API_KEY,
         'api-signature': generate_signature(method, endpoint, nonce, data)
     }
-
-
-def make_request(method, endpoint, params={}):
-    if params:
-        headers = generate_headers(
-            f"{method}", f"{endpoint}?{urlencode(params)}")
-    else:
-        headers = generate_headers(f"{method}", f"{endpoint}")
-    r = request(method, f"{BASE_URL}/{endpoint}",
-                params=params, headers=headers)
-    return r.json()
