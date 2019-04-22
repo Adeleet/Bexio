116680c4a27d382148b04df5a4e229149d01af30
Max Adaloudis <maxadaloudis@gmail.com>
2019-04-22 08:48:22 +0200

    Adds function to save bucketed trades to csv

--- generate_data.py
+++ generate_data.py
@@ -1,8 +1,49 @@
 import os
+import time
+from datetime import datetime
 from glob import glob
 
+import numpy as np
 import pandas as pd
 import requests
+
+from BitMEX import BitMEX
+
+client = BitMEX()
+
+
+def get_bucketed_trades():
+    client = BitMEX()
+    trade_data = np.empty((0, 12))
+    total_elapsed = (datetime.now() - datetime(2017, 1, 1))
+    total_minutes = int((total_elapsed.total_seconds() / 60))
+    progress = 0
+    columns = ['close', 'foreignNotional', 'high', 'lastSize', 'low', 'open',
+               'symbol', 'timestamp', 'trades', 'turnover', 'volume', 'vwap']
+    for i in range(0, total_minutes, 750):
+        time.sleep(0.8)
+        if "x-ratelimit-remaining" in client.response_headers.keys():
+            remaining = int(client.response_headers["x-ratelimit-remaining"])
+            current_progress = round(i / total_minutes, 2)
+            if current_progress > progress:
+                print(f"{round(i/total_minutes,2)} - {remaining}/300")
+                progress = current_progress
+            if remaining > 10 and remaining < 50:
+                time.sleep(30)
+            elif remaining < 10:
+                raise ValueError("exceeding rate limit")
+        new_values = pd.DataFrame(client.trades_bucketed(params={
+                                  "binSize": "1m",
+                                  "start": i,
+                                  "count": 750,
+                                  "symbol": "XBTUSD",
+                                  "columns": ",".join(columns)})).values
+        try:
+            trade_data = np.vstack([trade_data, new_values])
+        except ValueError:
+            print(client.rate_remaining, new_values)
+
+    return pd.DataFrame(trade_data, columns=columns)
 
 
 def download_data(source):
@@ -43,3 +84,7 @@ def create_dataframe(source):
             df_index += 1
             file_index = 0
         file_index += 1
+
+
+# data = get_bucketed_trades()
+# data.to_csv("data/bucketed/trades.csv.gz", index=False, compression="gzip")
